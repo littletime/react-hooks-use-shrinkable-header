c0306bdc91e633d9de7db53803f648d2a4115f80
Alexandre Rio <alexandre@charrette.club>
2022-05-19 17:10:23 +0200

    Added body position condition

--- useShrinkableHeader.js
+++ useShrinkableHeader.js
@@ -1,11 +1,20 @@
 import { useState, useEffect } from 'react'
 
+/*
+  * SHRINKED HEADER STATE CONDITION
+  *
+  * The "document.body.style.top" condition is here to keep shrinked header state when opening mobile menu with header being already shrinked
+  * With this condition, we can check if mobile menu is open (because mobile menu sets a body top property) AND if content has been scrolled
+  *
+  * TL;DR: without this condition, header always "unshrinks" when opening mobile menu
+  */
+
 export default (threshold = 20) => {
   const [shrinked, setShrinked] = useState(false)
-  
+
   function setHeaderSize() {
     const distanceY = window.pageYOffset
-    setShrinked(distanceY > threshold)
+    setShrinked(distanceY > threshold || document.body.style.top !== '0px') // see comment
   }
 
   useEffect(() => {
